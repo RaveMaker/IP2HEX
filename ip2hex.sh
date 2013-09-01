c93476f8a19a779aa4fbd54b7f7f11332acc05ba
Adam Mallul <RaveMaker@users.noreply.github.com>
2013-09-02 00:44:35 +0300

    Create ip2hex.sh

---
+++ ip2hex.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# convert IP addres to HEX value
+# For example : 10.0.0 - 0a0000
+
+echo "Please input SUBNET or IP, for example 10.0.0"
+read IP_ADDR
+
+# Convert values to Hex
+filename=$(printf '%02X' ${IP_ADDR//./ })
+
+# Convert to lowercase
+lowerfilename=${filename,,}
+
+# Print Upper and Lower Case Hex values
+echo $filename
+echo $lowerfilename

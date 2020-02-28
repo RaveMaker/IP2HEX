80322443fbd5867a8c832c822610ff2576a33e81
RaveMaker <ravemaker@gmail.com>
2020-02-28 02:29:54 +0200

    refactor

--- README.md
+++ README.md
@@ -3,16 +3,20 @@ IP2HEX
 
 Convert IP Address to Hex values
 
+Used to create PXE boot files specific to certain IP addresses
+
 ### Installation
 
-1. Clone this script from github or copy the files manually to your prefered directory.
+1. Clone this script from github or copy the files manually to your preferred directory.
 
 2. Run the script and input Subnet or IP Address.
 
 For Example:
 
-- 192.168.0 = C0A800
-- 10.0.0 = 0A0000
+```
+192.168.0 = C0A800
+10.0.0 = 0A0000
+```
 
 Author: [RaveMaker][RaveMaker].
 
--- ip2hex.sh
+++ ip2hex.sh
@@ -2,18 +2,19 @@
 
 # convert IP addres to HEX value
 # For example : 10.0.0 - 0a0000
+# used to create PXE boot files specific to certain IP addresses
 #
 # by RaveMaker - http://ravemaker.net
 
 echo "Please input SUBNET or IP, for example 10.0.0"
-read IP_ADDR
+read -r IP_ADDR
 
 # Convert values to Hex
-filename=$(printf '%02X' ${IP_ADDR//./ })
+HEX_IP_ADDR=$(printf '%02X' ${IP_ADDR//./ })
 
 # Convert to lowercase
-lowerfilename=${filename,,}
+HEX_IP_ADDR_LOWER=${HEX_IP_ADDR,,}
 
 # Print Upper and Lower Case Hex values
-echo $filename
-echo $lowerfilename
+echo "$HEX_IP_ADDR"
+echo "$HEX_IP_ADDR_LOWER"
